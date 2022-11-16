60defe83022633df31ac3a699266861bdce8efba
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-16 22:15:11 +0530

    [sourav]add : Nlog

---
+++ NLogProjecct/AddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProjecct
+{
+    internal class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if(firstNumber == 0|| secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Succesfull : Sum()");
+                nLog.LogError("expecting null values");
+                nLog.LogWarn("Second or secondNumber should not equal to 0");
+            }
+            int result = secondNumber + secondNumber;
+            nLog.LogDebug("Debug Successfully : Sum()");
+            nLog.LogInfo("Sum Method passed , Result "+ result);
+            return result;
+        }
+    }
+}
---
+++ NLogProjecct/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NLogProjecct
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1;
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("Result is : "+add.Sum(a,b));
+        }
+    }
+}
